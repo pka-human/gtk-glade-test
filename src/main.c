1e4420ebd80e775ae8e22552c21013be5cca8735
pka-human <o0o0.pe4Ka.0o0o@gmail.com>
2024-12-24 02:00:41 +0500

    calc

--- src/main.c
+++ src/main.c
@@ -2,9 +2,17 @@
 
 GtkBuilder *builder;
 
-void on_button1_clicked (GtkButton *b) {
+void on_button1_clicked(GtkButton *b) {
     GtkWidget *label1 = GTK_WIDGET(gtk_builder_get_object(builder, "label1"));
-    gtk_label_set_text(GTK_LABEL(label1), (const gchar*) "Hello, World!");
+    GtkAdjustment *adjustment1 = GTK_ADJUSTMENT(gtk_builder_get_object(builder, "adjustment1"));
+    GtkAdjustment *adjustment2 = GTK_ADJUSTMENT(gtk_builder_get_object(builder, "adjustment2"));
+
+    gdouble value1 = gtk_adjustment_get_value(adjustment1);
+    gdouble value2 = gtk_adjustment_get_value(adjustment2);
+
+    gchar *result_text = g_strdup_printf("%d + %d = %d", (int)value1, (int)value2, (int)(value1 + value2));
+    gtk_label_set_text(GTK_LABEL(label1), result_text);
+    g_free(result_text);
 }
 
 int main(int argc, char *argv[]) {
